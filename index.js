79429547e9df1f5fff82e7c2abd828acea82a2e9
Maddar <maddarr@bellsouth.net>
2022-04-11 15:38:56 -0500

    fixed JS functions

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-
+function certSlides() {
 let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+let i;
+let slides = document.getElementsByClassName("mySlides2");
+  for (i = 0; i < slides.length; i++) {
+  slides[i].style.display = "none"; 
+}
+    slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 6000); // Change image every 6 seconds
+}
+}
+
+function portSlides(){
+    let slideIndex = 0;
 showSlides();
 
 function showSlides() {
@@ -12,5 +29,5 @@ let slides = document.getElementsByClassName("mySlides1");
   if (slideIndex > slides.length) {slideIndex = 1} 
     slides[slideIndex-1].style.display = "block"; 
     setTimeout(showSlides, 6000); // Change image every 6 seconds
-
+}
 }
