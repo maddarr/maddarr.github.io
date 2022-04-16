9abf50187c2c242087694212f2b199ff89ed7868
Maddar <maddarr@bellsouth.net>
2022-04-16 13:50:23 -0500

    played with employment side

--- index.js
+++ index.js
@@ -17,67 +17,172 @@ function padding(n) {
 }
 
 function certSlides() {
-    let slideIndex = 0;
-    showSlides();
+  let slideIndex = 0;
+  showSlides();
     
-    function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-      for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
-    }
-        slideIndex++;
-      if (slideIndex > slides.length) {slideIndex = 0} 
-        slides[slideIndex-1].style.display = "block"; 
-        setTimeout(showSlides, 10000); // Change image every 10 seconds
-    }
-    }
-    function certSlides2() {
-        let slideIndex = 0;
-        showSlides();
+  function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 0} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 10000); // Change image every 10 seconds
+  }
+}
+
+function certSlides2() {
+  let slideIndex = 0;
+  showSlides();
         
-        function showSlides() {
-        let i;
-        let slides = document.getElementsByClassName("mySlides1");
-          for (i = 0; i < slides.length; i++) {
-          slides[i].style.display = "none"; 
-        }
-            slideIndex++;
-          if (slideIndex > slides.length) {slideIndex = 0} 
-            slides[slideIndex-1].style.display = "block"; 
-            setTimeout(showSlides, 10000); // Change image every 10 seconds
-        }
-        }
-    function certSlides3() {
-            let slideIndex = 0;
-            showSlides();
+  function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides1");
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 0} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 10000); // Change image every 10 seconds
+  }
+}
+
+function certSlides3() {
+  let slideIndex = 0;
+  showSlides();
             
-            function showSlides() {
-            let i;
-            let slides = document.getElementsByClassName("mySlides2");
-              for (i = 0; i < slides.length; i++) {
-              slides[i].style.display = "none"; 
-            }
-                slideIndex++;
-              if (slideIndex > slides.length) {slideIndex = 0} 
-                slides[slideIndex-1].style.display = "block"; 
-                setTimeout(showSlides, 10000); // Change image every 10 seconds
-            }
-            }
-            function portSlides(){
-              let slideIndex = 0;
-          showSlides();
+  function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides2");
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 0} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 10000); // Change image every 10 seconds
+  }
+}
+
+function portSlides(){
+  let slideIndex = 0;
+  showSlides();
           
-          function showSlides() {
-          let i;
-          let slides = document.getElementsByClassName("mySlides1");
-            for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none"; 
-          }
-              slideIndex++;
-            if (slideIndex > slides.length) {slideIndex = 1} 
-              slides[slideIndex-1].style.display = "block"; 
-              setTimeout(showSlides, 10000); // Change image every 10 seconds
-          }
-          }
+  function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides1");
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 10000); // Change image every 10 seconds
+  }
+}
+
+function USPS() {
+  let slideIndex = 0;
+  showSlides();
+            
+  function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("USPS");
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 0} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 20000); // Change image every 10 seconds
+  }
+}
+
+function LOBBS() {
+  let slideIndex = 0;
+  showSlides();
+              
+  function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("LOBBS");
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 0} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 20000); // Change image every 10 seconds
+  }
+}
+
+function CMI() {
+  let slideIndex = 0;
+  showSlides();
+                
+  function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("CMI");
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 0} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 20000); // Change image every 10 seconds
+  }
+}
+
+function FSL() {
+  let slideIndex = 0;
+  showSlides();
+                  
+  function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("FSL");
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 0} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 20000); // Change image every 10 seconds
+  }
+}
+
+function LOWES() {
+  let slideIndex = 0;
+  showSlides();
+                    
+  function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("LOWES");
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 0} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 20000); // Change image every 10 seconds
+  }
+}  
+
+function THD() {
+  let slideIndex = 0;
+  showSlides();
+                      
+  function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("THD");
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 0} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 20000); // Change image every 10 seconds
+  }
+}      
     
