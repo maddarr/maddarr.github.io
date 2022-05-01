a08142ff73a86c9294ad92380c3a451276e9dc08
Maddar <maddarr@bellsouth.net>
2022-05-01 08:10:37 -0500

    yep

---
+++ index_test.js
@@ -0,0 +1,12 @@
+import assert from 'assert';
+const assert = require('assert');
+const employmentChoice = require('./index');
+
+describe('employmentChoice', () => {
+    describe('choiceId', () => {
+        it('returns a style height', () => {
+            const expectedResult = '2px';
+            assert.strictEqual(expectedResult);
+        });
+    });
+});
