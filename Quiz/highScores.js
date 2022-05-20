a87b150b03b8183af001579aba5994b3ddc66cca
Maddar <maddarr@bellsouth.net>
2022-05-20 18:29:40 -0500

    fixed game two score

--- Quiz/highScores.js
+++ Quiz/highScores.js
@@ -1,7 +1,7 @@
 const gameOneFinal = document.getElementById('g1Final');
 const gameTwoFinal = document.getElementById('g2Final');
 const gameOneScore = sessionStorage.getItem('gameOneScore');
-const gameTwoScore = sessionStorage.getItem('mostRecentScore');
+const gameTwoScore = sessionStorage.getItem('gameTwoScore');
 
 gameOneFinal.innerText = gameOneScore;
 gameTwoFinal.innerText = gameTwoScore;
